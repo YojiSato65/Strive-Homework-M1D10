0b64ace28021a6949aa74e10d6305658dd6a3828
YojiSato65 <yojisato65@gmail.com>
2022-02-04 17:49:33 +0200

    Solved ex.36

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 /*JS EXERCISES
 
 21) Given variables x = "John" and y = "Doe", write on the console "John <> Doe"
@@ -53,6 +54,8 @@ for (let i = 0; i <= 99; i ++) {
 console.log(ranNumArr)
 
 console.log('\n-----------ex.27----------')
+// not sure how to use an array in the function
+
 // const ranNums = function (){
 //     let ranNumArr = []
 //     for (let i = 0; i <= 99; i ++) {
@@ -66,15 +69,56 @@ console.log('\n-----------ex.27----------')
 
 // }
 
-
 console.log('\n-----------ex.28----------')
-let arrOfArr = [
-let childArr = []
-    for (let i = 0; i <= 9; i ++) {
-        let r = Math.floor(Math.random() * 10) + 1
-        childArr.push(r)
-    } 
-    let 10Arr =  childArr * 10
+//not sure how to create an array of an array
+// let arrOfArr = []
+// let childArr = []
+//     for (let i = 0; i <= 9; i ++) {
+//         let r = Math.floor(Math.random() * 10) + 1
+//         childArr.push(r)
+//     } 
+//     let Arr10 =  childArr * 10
+
+// console.log(arrOfArr)
+
+console.log('\n-----------ex.29----------')
+console.log('\n-----------ex.30----------')
+
+
+//DOM EXERCISES//
+
+//31) Get the element with an id of "container" from the page //
+let getContainer = document.getElementById('div-container')
+//32) Get every <td> element from the page //
+let allTds = document.getElementsByTagName('td')
+//33) Use a loop for printing the text inside of every <td> element in the page//
+for (let i = 0; i < allTds.length; i ++) {
+    console.log(allTds(i))
 }
-]
-console.log(arrOfArr)
+//34) Write a function to change the heading of the page//
+const changeHeader = function () {
+    let header = document.querySelector('h1')
+    header.innerText = 'I changed the header!'
+}
+changeHeader()
+//35) Write a function to add an extra row to the table//
+let newRow = document.createElement('tr')
+newRow.innerText = 'I am the new row!'
+document.querySelector('table').appendChild('newRow')
+//36) Write a function to add a class of "test" to each row in the table//
+const setTrToTest = function () {
+    let allTr = document.getElementsByTagName('tr')
+    for (let i = 0; i < allTr.length; i ++) {
+        let eachTr = allTr(i)
+        eachTr.classList.add('test')
+    }
+}
+setTrToTest()
+
+//37) Write a function to add a red background to every link in the page//
+//38) Console log "Page loaded" when the page is correctly loaded//
+//39) Write a function to add new items to a unordered list//
+//40) Write a function to empty a list//
+
+
+
